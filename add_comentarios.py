28e2f15d829aaa18b547e49b227417e7b9299059
MauricioBecerra <a21310105@ceti.mx>
2024-03-01 22:08:27 -0600

    Update script "add_comentarios"
    Se agrego la segunda version de un script que comentara el codigo original, generando una copia con esos comentarios agregados,retiramos la paqueteria ast y utilizamos for e if para encontrar la linea similar.

--- add_comentarios.py
+++ add_comentarios.py
@@ -1,37 +1,17 @@
-import ast
-
 archivo_original = "main.py"
 archivo_nuevo = "mainconcomentarios.py"
 
 with open(archivo_original, "r") as f:
-    codigo = f.read()
+    lineas = f.readlines()
 
-arbol_org = ast.parse(codigo)
+# Agregar comentarios después de las llamadas a la función print
+for i, linea in enumerate(lineas):
+    if "print(" in linea:
+        lineas[i] = linea.rstrip() + " # Se imprime un texto con un print\n"
+    
 
-class AgregarComentarios(ast.NodeTransformer):
-
-    def visit(self, node):
-        if isinstance(node, ast.If) or (isinstance(node, ast.Call) and isinstance(node.func, ast.Name) and node.func.id == "print"):
-            comentario = ast.Expr(value=ast.Str(s=f"# Se hace un {node.__class__.__name__.lower()}"))
-            # Creamos un nodo que contenga el nodo original y el comentario como una tupla
-            nodo = ast.Expr(value=ast.Tuple(elts=[node, comentario], ctx=ast.Load()))
-            # No llamamos a self.generic_visit(nodo) dentro del bloque if
-            return nodo
-
-        # Si no estamos en un nodo que necesita comentarios, se llama a generic_visit
-        return self.generic_visit(node)
-
-# Se crea una instancia de la clase AgregarComentarios
-transformador = AgregarComentarios()
-
-# Se aplica el transformador al árbol sintáctico
-arbol_modificado = transformador.visit(arbol_org)
-
-# Convertimos el árbol modificado a código python
-codigo_modificado = ast.unparse(arbol_modificado)
-
-# Abre el nuevo archivo para escribir el código con los comentarios
+# Guardar las líneas modificadas en el nuevo archivo
 with open(archivo_nuevo, "w") as f:
-    f.write(codigo_modificado)
+    f.writelines(lineas)
 
-print(f"Se ha creado el archivo {archivo_nuevo} con los comentarios.")
+print(f"Se ha creado el archivo {archivo_nuevo} con comentarios después de las llamadas a la función print.")
